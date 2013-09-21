882914770771f36f97fec79ff5a2217ac8773138
Wylie Fisher <wylie@dukeofcheese.com>
2013-09-21 14:05:54 -0400

    npm installing things

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+
+	// project config
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		php: {
+			test: {
+				options: {
+					port: 7007,
+					hostname: 'localhost',
+					base: 'dev/',
+					keepalive: true,
+					open: true
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks("grunt-php");
+	grunt.loadNpmTasks("grunt-contrib-less");
+	grunt.loadNpmTasks("grunt-contrib-watch");
+
+	grunt.registerTask("compile", [
+		"less"
+	]);
+
+	grunt.registerTask("server", [
+		"php"
+	]);
+	
+};
